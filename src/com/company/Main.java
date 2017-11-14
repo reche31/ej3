c709889e6c6a431bdbd0c7254912efa7dd26cac1
Cristian <cristianrf96@gmail.com>
2017-11-14 13:27:10 +0100

    data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 	    Scanner teclat = new Scanner(System.in);
-	    temps[] marca = new temps[10];
-	    temps[] resultat = new temps[10];
+	    temps[] marca = new temps[5];
+
+	    int mitjana;
+	    int sum=0;
 
         for (int i =0;i<marca.length;i++) {
             marca[i] = new temps();
@@ -15,8 +17,22 @@ public class Main {
             System.out.println("Tecleja el temps");
             marca[i].temps = teclat.nextFloat();
 
-            System.out.println(marca[i].nom);
+            teclat.nextLine();
+        }
 
+        for (int i =0;i<marca.length;i++){
+            sum += marca[i].temps;
+        }
+        mitjana = sum/10;
+
+        for (int i =0;i<marca.length;i++){
+            if(marca[i].temps > 15){
+                System.out.println("La persona "+marca[i].nom+" ha tardat més de 15 mins min");
+            }else if(marca[i].temps < mitjana){
+                System.out.println("La persona "+marca[i].nom+" ha estat per sota de la mitjana");
+            }else {
+                System.out.println("La persona" + marca[i].nom + "A tardat menys de 15 mins i esta per sobra de la mitjana");
+            }
         }
     }
 }
